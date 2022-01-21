d8a39184889f705e4b17c517ad803ed2f2f0574a
jalopezqv <jalopezqvision@gmail.com>
2022-01-21 15:24:16 -0500

    Se realiza la creacion del proyecto inicial en el cual se ha tenido en cuenta una organizacion del archivo settings.py, la creacion de la carpeta de aplicaciones y la configuracion inicial del proyecto

---
+++ sistemaVenta/sistemaVenta/settings/prod.py
@@ -0,0 +1,50 @@
+from base import *
+
+# Application definition
+
+DJANGO_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+LOCAL_APPS = [
+    'applications.home',
+]
+
+THIRD_PARTY_APPS = []
+
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bdsistemaventa',
+        'USER':'userSistemaVenta',
+        'PASSWORD':'userSistemaVenta123',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
